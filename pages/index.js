ff62ef14b84a1d34f10be6531b9b276e59cf608f
haruyasu <harukun2002@gmail.com>
2021-03-31 21:32:49 +0900

    layout

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,18 @@ export default function Home({ posts }) {
       <Head>
         <title>NextJS Startup</title>
       </Head>
-      <div className="mb-5">
+      <div className="mb-10 p-3 border rounded">
         <Image
-          className="object-cover"
+          className="object-cover rounded"
           src="/top.png"
           alt="top"
           width={1280}
           height={500}
         />
+      </div>
+      <div className="flex justify-center flex-col items-center mb-10">
+        <div className="text-lg mb-3">BLOG POSTS</div>
+        <div className="border w-14"></div>
       </div>
       <div className="flex flex-wrap -m-4 mb-5">
         {posts && posts.map((post) => <Post key={post.id} post={post} />)}
